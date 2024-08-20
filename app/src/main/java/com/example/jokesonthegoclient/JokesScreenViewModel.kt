11275b018294713fae4362cf03a2c7049773bf79
stephen-bapple <stephen.bapple@gmail.com>
2024-08-20 13:44:01 -0600

    feat: make UI a little more clean looking

--- app/src/main/java/com/example/jokesonthegoclient/JokesScreenViewModel.kt
+++ app/src/main/java/com/example/jokesonthegoclient/JokesScreenViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.github.stephenbapple.jokesonthegoservice.GetAnyRandomJokeResponse
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -15,11 +16,17 @@ class JokesScreenViewModel @Inject constructor(
 ): ViewModel() {
     private val _jokeResponse = MutableStateFlow<GetAnyRandomJokeResponse?>(null)
     val jokeResponse: StateFlow<GetAnyRandomJokeResponse?> = _jokeResponse
+    private val _isLoading = MutableStateFlow<Boolean>(false)
+    val isLoading: StateFlow<Boolean> = _isLoading
 
     fun getRandomJoke() {
         viewModelScope.launch {
+            _isLoading.value = true
             val res = rpcClient.getRandomJoke()
+            delay(500)
+
             _jokeResponse.value = res
+            _isLoading.value = false
         }
     }
 }
--- app/src/main/java/com/example/jokesonthegoclient/MainActivity.kt
+++ app/src/main/java/com/example/jokesonthegoclient/MainActivity.kt
@@ -6,22 +6,37 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jokesonthegoclient.ui.theme.JokesOnTheGoClientTheme
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.HiltAndroidApp
@@ -51,19 +66,22 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun JokesScreen(viewModel: JokesScreenViewModel, modifier: Modifier = Modifier) {
-    val theJoke = viewModel.jokeResponse.collectAsState()
+    val theJoke by viewModel.jokeResponse.collectAsState()
+    val isLoading by viewModel.isLoading.collectAsState()
+
     Column(
         modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        val jokeText = if (theJoke.value == null) {
-            "Press the button!"
+        val jokeText = if (theJoke == null) {
+            ">..."
         } else {
-            "${theJoke.value?.joke?.setup}\n\n${theJoke.value?.joke?.punchline}"
+            "${theJoke?.joke?.setup}\n\n${theJoke?.joke?.punchline}"
         }
         JokeRetrieverCard(
             jokeText = jokeText,
+            enabled = !isLoading,
             jokeUpdateFunc = { viewModel.getRandomJoke() })
     }
 }
@@ -72,23 +90,84 @@ fun JokesScreen(viewModel: JokesScreenViewModel, modifier: Modifier = Modifier) 
 fun JokeRetrieverCard(
     jokeText: String,
     modifier: Modifier = Modifier,
-    jokeUpdateFunc: () -> Unit
+    enabled: Boolean = false,
+    jokeUpdateFunc: () -> Unit = {},
 ) {
+    val metallicBlueSweep = listOf(
+        Color(0xFFB0CFE5), // Light blue-gray
+        Color(0xFF90B5BE), // Medium blue-gray
+        Color(0xFF78A1AC), // Dark blue-gray
+        Color(0xFFB0CFE5)  // Light blue-gray again to create the sweep effect
+    )
     Column(
         modifier = modifier
-            .background(color = Color.DarkGray)
-            .fillMaxSize(.75f)
-            .padding(16.dp),
+            .background(brush = Brush.sweepGradient(metallicBlueSweep))
+            .fillMaxSize()
+            .shadow(4.dp, shape = RoundedCornerShape(16.dp)),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = jokeText
-        )
-        Button(onClick = jokeUpdateFunc) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(.75f)
+                .fillMaxHeight(0.30f),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
             Text(
-                text="Get random joke"
+                text = jokeText,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Green,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(color = Color(0xFF252525), shape = RoundedCornerShape(16.dp))
+
+                    .border(1.dp, Color.Black, shape = RoundedCornerShape(16.dp))
+                    .padding(16.dp)
+                    .weight(1f),
             )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                onClick = jokeUpdateFunc,
+                enabled = enabled,
+                shape = RoundedCornerShape(4.dp),
+                border = BorderStroke(1.dp, Color(0xFF252525)),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(alignment = Alignment.End)
+                    .height(48.dp),
+                elevation = ButtonDefaults.buttonElevation(
+                    defaultElevation = 16.dp,
+                    pressedElevation = 4.dp,
+                    disabledElevation = 0.dp,
+                    hoveredElevation = 8.dp,
+                    focusedElevation = 8.dp
+                ),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF43B6A5),
+                    contentColor = Color.White
+                )
+            ) {
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    if (enabled) {
+                        Text(
+                            text = "Get a random joke"
+                        )
+                    } else {
+                        CircularProgressIndicator(
+                            color = Color.Green,
+                            modifier = Modifier
+                                .height(32.dp)
+                                .width(32.dp)
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text(text = "loading...")
+                    }
+                }
+            }
         }
     }
 }
@@ -99,6 +178,12 @@ fun PreviewJokeRetrieverCard() {
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
-        JokeRetrieverCard(jokeText = "Why did the user cross the screen?\n\nTo get to the other slide") {}
+        val aJoke = "Why did the user cross the screen?\n" +
+                "\n" +
+                "To get to the other slide"
+        JokeRetrieverCard(
+            jokeText = aJoke,
+            enabled = true
+        ) {}
     }
 }
